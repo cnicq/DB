f59db2d24145c743b0a82d82d82490c0ca286106
cnicq <swjtuboy@163.com>
2013-07-08 18:12:41 +0800

    - add new data

--- stardb.py
+++ stardb.py
@@ -1,3 +1,4 @@
+
 import os
 import subprocess
 import time
@@ -7,13 +8,11 @@ nginx_path = "C:\\Git\Software\\nginx-1.5.0";
 dataapp_path = "C:\\Git\\DB";
 php_path = "C:\\Git\\software\\php-5.4.16-nts-Win32-VC9-x86";
 
-#shut down nignx
-nginxstar_cmd = "" + nginx_path + "\\nginx.exe -s quit";
-print("stop nginx... "+ nginxstar_cmd);
-os.system(nginxstar_cmd);
-time.sleep(2)
-
-#shut down php
+if True:
+	mongodb_path = "E:\\Study\\Web\\mongodb-win32-i386-2.4.3";
+	nginx_path = "E:\\Study\\Web\\nginx-1.4.1";
+	dataapp_path = "E:\\Study\\Web\\Root\\DB";
+	php_path = "E:\\Study\\Web\\php-5.4.16-nts-Win32-VC9-x86";
 
 mongodbstar_cmd = "" + mongodb_path + "\\bin\\mongod --dbpath " + dataapp_path + "\\data"
 print("star mongodb... " + mongodbstar_cmd);
@@ -23,5 +22,6 @@ phpstar_cmd = "" + php_path + "\\php-cgi -b 127.0.0.1:9000 -c " + php_path + "\\
 print("star php... " + phpstar_cmd);
 subprocess.Popen(phpstar_cmd, shell=True) 
 time.sleep(3)
-
+nginxstar_cmd = "" + nginx_path + "\\nginx.exe -p " + nginx_path;
+print("star nginx... "+ nginxstar_cmd);
 subprocess.Popen(nginxstar_cmd, shell=True) 
