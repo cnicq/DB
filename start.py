206e697550da535d1a62fa92f06e77ad9acf8fe4
cnicq <swjtuboy@163.com>
2013-07-29 18:08:14 +0800

    - DB node app

--- start.py
+++ start.py
@@ -8,7 +8,7 @@ nginx_path = "C:\\Git\Software\\nginx-1.5.0";
 dataapp_path = "C:\\Git\\DB";
 php_path = "C:\\Git\\software\\php-5.4.16-nts-Win32-VC9-x86";
 
-if False:
+if True:
 	mongodb_path = "E:\\Study\\Web\\mongodb-win32-i386-2.4.3";
 	nginx_path = "E:\\Study\\Web\\nginx-1.4.1";
 	dataapp_path = "E:\\Study\\Web\\Root\\DB";
@@ -25,3 +25,7 @@ time.sleep(3)
 nginxstar_cmd = "" + nginx_path + "\\nginx.exe -p " + nginx_path;
 print("star nginx... "+ nginxstar_cmd);
 subprocess.Popen(nginxstar_cmd, shell=True) 
+time.sleep(3)
+node_cmd = "node app.js"
+print("star node... "+ node_cmd);
+subprocess.Popen(node_cmd, shell=True) 
