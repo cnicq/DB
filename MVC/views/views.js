641c832734418be8f27adf249a64990f68c3f9a4
cnicq <swjtuboy@163.com>
2013-07-30 18:19:44 +0800

    -d3js support.

---
+++ MVC/views/views.js
@@ -0,0 +1,13 @@
+var ChatView = Backbone.View.extend({
+    tagName: 'li',
+
+    initialize: function(options) {
+        _.bindAll(this, 'render');
+        this.model.bind('all', this.render);
+    },
+
+    render: function() {
+        $(this.el).html(this.model.get("name") + ": " + this.model.get("text"));
+        return this;
+    }
+});
