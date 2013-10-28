1d6830cafdbabcd8d4fbb35de51fb3390e503826
Justin <swjtuboy@163.com>
2013-10-28 18:14:19 +0800

    - time format for x axis.

--- public/d3.custom.js
+++ public/d3.custom.js
@@ -428,7 +428,7 @@ function ShowLineChart() {
 
 var customTimeFormat = timeFormat([
   [d3.time.format("%Y"), function() { return true; }],
-  [d3.time.format("%B"), function(d) { return d.getMonth(); }],
+  [function(d){ return moment(d).format('YYYY-MM')}, function(d) { return d.getMonth(); }],
   [d3.time.format("%b %d"), function(d) { return d.getDate() != 1; }],
   [d3.time.format("%a %d"), function(d) { return d.getDay() && d.getDate() != 1; }],
   [d3.time.format("%I %p"), function(d) { return d.getHours(); }],
@@ -436,6 +436,7 @@ var customTimeFormat = timeFormat([
   [d3.time.format(":%S"), function(d) { return d.getSeconds(); }],
   [d3.time.format(".%L"), function(d) { return d.getMilliseconds(); }]
 ]);
+
 function timeFormat(formats) {
   return function(date) {
     var i = formats.length - 1, f = formats[i];
